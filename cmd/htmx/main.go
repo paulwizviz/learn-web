f3c21419b141be9c0180bdbc61494a48e53585a2
Paul Sitoh <paulwizviz@gmail.com>
2024-12-22 16:04:47 +0000

    Update HTMX
    Include example of demonstrating the use of Tabs.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- cmd/htmx/main.go
+++ cmd/htmx/main.go
@@ -63,12 +63,27 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(fileContent)
 }
 
+func tab1HDL(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<p style="color:DodgerBlue;" >Hello Tab 1</p>`))
+}
+
+func tab2HDL(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<p style="color:MediumSeaGreen;">Hello Tab 2</p>`))
+}
+
+func tab3HDL(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<p style="color:Tomato;">Hello Tab 3</p>`))
+}
+
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", homeHDL)
 	mux.HandleFunc("GET /static/htmx.min.js", staticHandler)
 	mux.HandleFunc("GET /test", testHDL)
+	mux.HandleFunc("GET /tab1", tab1HDL)
+	mux.HandleFunc("GET /tab2", tab2HDL)
+	mux.HandleFunc("GET /tab3", tab3HDL)
 	mux.HandleFunc("POST /data", dataHDL)
 	log.Printf("Starting web %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), mux))
